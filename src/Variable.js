b4102aa63d0f1e827ef40020d2dc5892158a5cb0
liar780@gmail.com <liar780@gmail.com>
2022-10-12 18:42:48 +0700

    belajar variable

---
+++ src/Variable.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+// const
+// const harga=3000;
+// harga =2000 // salah karena nilai const tidak bisa dii ubah
+
+// var
+// var harga = 2000; //var di akan tembus scoop
+// if (true) {
+//  var harga = 1000;
+// }
+
+// let 
+// let harga =2000 // let di hadang oleh scoop 
+// if(true){
+//   let harga =1000
+// }
+
+
+
+function Variable() {
+  return (
+    <div>
+      Variable
+      <p>cek harga : {harga}</p>
+    </div>
+  );
+}
+export default Variable;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+import Variable from './Variable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Variable />
+    {/* <App /> */}
   </React.StrictMode>
 );
